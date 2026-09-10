d77b5fb77a2a85013925e3c486eea06d39b9c7ad
Dheyslow Manullang <manullangdeslow@gmail.com>
2026-09-11 00:02:38 +0700

    Coding setup cors backend

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
+        $middleware->statefulApi();
+
         $middleware->alias([
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
