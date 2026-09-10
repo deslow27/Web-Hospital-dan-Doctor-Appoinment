a95277f3474fb73f4f24d2a0143bf62f1a79c135
Dheyslow Manullang <manullangdeslow@gmail.com>
2026-09-10 20:37:40 +0700

    Coding auth service controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\AuthService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\LoginRequest;
+
+class AuthController extends Controller
+{
+    //
+    private $authService;
+
+    public function __construct(AuthService $authService)
+    {
+        $this->authService = $authService;
+    }
+
+    public function register(RegisterRequest $request)
+    {
+        $user = $this->authService->register($request->validated());
+        return response()->json(['message' => 'User registered successfully', 'user' => $user], 201);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        return $this->authService->login($request->validated());
+    }
+
+    public function tokenLogin(LoginRequest $request)
+    {
+        return $this->authService->tokenLogin($request->validated());
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return response()->json(['message' => 'Logged out successfully']);
+    }
+}
---
+++ app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name'     => 'required|string|max:255',
+            'email'    => 'required|email|unique:users,email',
+            'phone'    => 'required|string|max:20',
+            'gender'   => 'required|string|max:20',
+            'photo'    => 'required|image|mimes:jpeg,png,jpg|max:2048',
+            'password' => 'required|string|min:8|confirmed',
+        ];
+    }
+}
